f21d0a0acdafadf3efdb4885545aa1dd0352dacf
Sahil1109 <sahil.jangra@block8.com>
2020-02-12 10:56:10 +0530

    1st commit

---
+++ test/helloWorld.js
@@ -0,0 +1,10 @@
+// pragma solidity ^0.5.16;
+const HelloWorld = artifacts.require('HelloWorld');
+
+contract ('HelloWorld',()=>{
+    it('Should return hello world', async()=>{
+    const helloWorld = await HelloWorld.deployed();
+    const result = await helloWorld.hello();
+    assert(result === 'Hello world')
+    });
+});
